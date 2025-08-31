681163b64f594b8af6d763b3ceae19b88dd79960
Jibaru <ignacioruedaboada@hotmail.com>
2025-08-31 10:35:39 -0500

    feat: add MySQL and Postgres DAO generation tests

---
+++ internal/generator/mysql_test.go
@@ -0,0 +1,50 @@
+package generator_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Jibaru/gormless/internal/generator"
+	"github.com/Jibaru/gormless/internal/parser"
+)
+
+func TestGenerateMySQLDAO(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		model := parser.Model{
+			Name: "User",
+			Fields: []parser.Field{
+				{Name: "ID", Type: "int", Column: "id", IsPrimary: true},
+				{Name: "Name", Type: "string", Column: "name"},
+				{Name: "Email", Type: "*string", Column: "email"},
+				{Name: "Password", Type: "string", Column: "password"},
+				{Name: "Age", Type: "int", Column: "age"},
+				{Name: "DeletedAt", Type: "*time.Time", Column: "deleted_at"},
+			},
+			TableName:  "users",
+			PrimaryKey: "ID",
+			Package:    "models",
+			ImportPath: "github.com/someone/models",
+		}
+
+		content, err := generator.GenerateMySQLDAO(model)
+
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		file, err := os.Open("data/mysql_stub.txt")
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		defer file.Close()
+
+		expectedContent, err := os.ReadFile("data/mysql_stub.txt")
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		if string(expectedContent) != content {
+			t.Errorf("expected %s, got %s", string(expectedContent), content)
+		}
+	})
+}
